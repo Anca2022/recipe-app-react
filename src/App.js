18eb4410a800f1890475f46bb574eeb60fe06222
Anca Matei <ancamatei2012@gmail.com>
2024-06-05 23:44:01 +0300

    homepage - html&css done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
+import RecipeCard from './components/RecipeCard';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Navbar />
+      
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>     
+        <Route path="/cuisines" element={<RecipeCard/>}></Route>     
+        <Route path="/about" element={<RecipeCard/>}></Route>     
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 import '../App.css'; 
+import { Link } from 'react-router-dom';
 
 export default function Navbar () {
     return(
         <div className="navbar">
             <div className="logo">Recipes</div>
             <ul className="navigation">
-                <li>Meals</li>
-                <li>Cuisines</li>
-                <li>About</li>
+                <li><Link to="/">Meals</Link></li>
+                <li><Link to="/cuisines">Cuisines</Link></li>
+                <li><Link to="/about">About</Link></li>
             </ul>
-        </div>  
-        
+        </div>     
     )
 }
